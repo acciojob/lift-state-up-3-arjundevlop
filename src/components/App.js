54484b1dc98423b471d58f6d432da0e9c6885286
arjundevlop <arjun.tomar45@gmail.com>
2023-04-28 17:49:59 +0530

    submit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [data, setdata] = useState("");
+
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="parent">
+    <div className="tomato-child">
+      <h2>Child Component 1</h2>
+      <button onClick={()=>{setdata("Option 1")}}>Option 1</button>
+    </div>
+
+    <div className="yellow-child">
+      <h2>Child Component 2</h2>
+      <button onClick={()=>{setdata("Option 2")}}>Option 2</button>
+    </div>
+
+    <div className="answer">
+    Selected Option: {data}
+
+    </div>
+        
     </div>
   )
 }
