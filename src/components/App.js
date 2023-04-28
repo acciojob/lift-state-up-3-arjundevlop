58652cb15358ac34fb7b888a14cf575e52272c6c
arjundevlop <arjun.tomar45@gmail.com>
2023-04-28 18:01:09 +0530

    submit2

--- src/components/App.js
+++ src/components/App.js
@@ -11,12 +11,12 @@ const App = () => {
     <div className="parent">
     <div className="tomato-child">
       <h2>Child Component 1</h2>
-      <button onClick={()=>{setdata("Option 1")}}>Option 1</button>
+      <button onClick={(e)=>{setdata(e.target.innerHTML)}}>Option 1</button>
     </div>
 
     <div className="yellow-child">
       <h2>Child Component 2</h2>
-      <button onClick={()=>{setdata("Option 2")}}>Option 2</button>
+      <button onClick={(e)=>{setdata(e.target.innerHTML)}}>Option 2</button>
     </div>
 
     <div className="answer">
